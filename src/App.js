45541986033d57fc5b0a8474178a70adb0688cf3
Jeffrey Platt <JSPlatt@gmail.com>
2021-04-18 12:48:16 -0400

    working on Header

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="app">
+
+     <Header/>
+    
+    {/*  {Body /} */}
+        {/*  {Sidebar /} */}
+        {/*  {Feed /} */}
+        {/*  {Widget /} */}
+    </div>
+  );
+}
+
+export default App;
