b86c6736d6f11702f040fe290d5daeaaea568752
Jeffrey Platt <JSPlatt@gmail.com>
2021-04-19 11:32:14 -0400

    Widget Started

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Widgets from './Widgets';
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
      <div className="app_body">
         <Sidebar />
         <Feed />
-        {/*  {Widget /} */}
+        <Widgets />
 
      </div>
     
